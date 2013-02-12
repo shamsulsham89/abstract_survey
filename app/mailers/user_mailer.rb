95942230ea7fcccee4c687d87d6d87f1cf57a34e
Shamsul Haque <shamsulsham89@gmail.com>
2013-02-12 13:37:41 +0530

    Update app/mailers/user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "shamsulsham89@gmail.com"
+  default from: "abstractsurvey@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -7,7 +7,7 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.password_reset.subject
   #
   def password_reset(user)
-  @user = user
-  mail :to => user.email, :subject => "Password Reset"
-end
+    @user = user
+    mail :to => user.email, :subject => "Password Reset"
+  end
 end
