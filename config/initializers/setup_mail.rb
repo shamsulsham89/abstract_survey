c06c7d381dfe58f93fae1ff59ff1791817577984
Shamsul Haque <shamsulsham89@gmail.com>
2013-02-16 20:41:03 +0530

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,15 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port  => 587,
-  :domain  => 'http://fathomless-earth-2350.herokuapp.com',
+  :domain  => 'fathomless-earth-2350.herokuapp',
   :user_name => "abstractsurvey@gmail.com.",
-  :password => "",
+  :password => "abstract123#",
   :authentication => 'plain',
   :enable_starttls_auto => true
 }
 
 ActionMailer::Base.default_url_options[:host] = "http://fathomless-earth-2350.herokuapp.com"
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+
+
 
