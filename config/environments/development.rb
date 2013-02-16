b909d2aa32257098fa52508710493897c220e974
Shamsul Haque <shamsulsham89@gmail.com>
2013-02-16 19:52:56 +0530

    Update config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,5 @@ AbstractSurvey::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "lohttp://fathomless-earth-2350.herokuapp.com" }
 end
