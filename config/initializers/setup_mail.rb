09f2419bb380c70fa286b6d2392132947d72338d
Shamsul Haque <shamsulsham89@gmail.com>
2013-02-16 22:54:02 +0530

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port  => 587,
-  :domain  => 'gmail.com',
   :user_name => "abstractsurvey@gmail.com.",
   :password => "abstract123#",
   :authentication => 'plain',
