92185af58b4764ec0168a47da3fdd2384528cd36
shamsul <shamsul@shamsul-VPCEH28FN>
2013-02-17 02:55:23 +0530

    hello

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,7 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port  => 587,
