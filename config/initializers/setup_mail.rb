1a31d392630a9bdf16b2a362740e576e1aa40960
Shamsul Haque <shamsulsham89@gmail.com>
2013-02-16 20:07:28 +0530

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port  => 587,
   :domain  => 'http://fathomless-earth-2350.herokuapp.com',
   :user_name => "abstractsurvey@gmail.com.",
-  :password => "abstract123#",
+  :password => "",
   :authentication => 'plain',
   :enable_starttls_auto => true
 }
