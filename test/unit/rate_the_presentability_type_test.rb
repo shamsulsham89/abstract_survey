a71a558640e2a70658f572f3c099fecfa2d12d0b
shamsul <shamsul@shamsul-VPCEH28FN.(none)>
2013-02-24 05:10:36 +0530

    firs commit

---
+++ test/unit/rate_the_presentability_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RateThePresentabilityTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/stock_availability_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StockAvailabilityTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
